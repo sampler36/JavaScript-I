76a082520e4d8528f6ce4b80906bf9e471ff6aae
Talent Antonio <antonio.talenty@gmail.com>
2019-01-21 12:20:45 +0000

    Setting my Environment

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -56,10 +56,10 @@ let inventory = [{"id":1,"car_make":"Lincoln","car_model":"Navigator","car_year"
 
 // Example for loop:
 
-// arr = [1,2,3,4];
-// for (i = 0; i < arr.length; i++) {
-//     arr[i]; // 1,2,3,4
-// }
+arr = [1,2,3,4];
+for (i = 0; i < arr.length; i++) {
+    arr[i]; // 1,2,3,4
+}
 
 // ==== Challenge 1 ====
 // The dealer can't recall the information for a car with an id of 33 on his lot. Help the dealer find out which car has an id of 33 by logging the car's year, make, and model in the console log provided to you below:
